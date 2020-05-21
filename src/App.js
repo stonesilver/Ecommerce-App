48eed8c29049a0e505a1b7067c71ef5ab3561359
Ezenwa Chigozie <stonesilver12@gmail.com>
2020-05-21 06:47:58 +0100

    updating our app with a shop page, collection preview and item components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import shopPage from './pages/shop/shop.component';
 
 function App() {
    return (
       <div>
-         <Homepage />
+         <Switch>
+            <Route exact path='/' component={Homepage} />
+            <Route exact path='/shop' component={shopPage} />
+         </Switch>
       </div>
    );
 }
